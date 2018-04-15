a9d45e7ee002ab31ddf97003ea7419e32256724a
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2018-04-15 22:51:23 +0100

    make passphrase storage more secure

--- modules/ssh/init-ssh.sh
+++ modules/ssh/init-ssh.sh
@@ -5,6 +5,7 @@ then
 	echo "Private key is present, skipping ssh-keygen stage"
 else
     passphrase=$( gpg --gen-random --armor 1 20 )
-    echo "$passphrase" > ~/.ssh/passthrase
+    echo "$passphrase" > ~/.ssh/.secret
+    chmod 0600 > ~/.ssh/.secret
 	ssh-keygen -N $passphrase -f ~/.ssh/id_rsa
 fi
