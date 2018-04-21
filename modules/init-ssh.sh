815ed1b094bc3cfd3113e180a94685da53758ac4
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2018-04-21 23:49:47 +0100

    First version was running incorrect file names

--- modules/init-ssh.sh
+++ modules/init-ssh.sh
@@ -5,22 +5,22 @@ echo "init SSH module"
 echo "list of present keys for the server:"
 for keyfile in /etc/ssh/ssh_host*; do ssh-keygen -l -f "${keyfile}"; done | uniq
 
-if [ -f /etc/ssh/id_rsa ]
+if [ -f /etc/ssh/ssh_host_rsa ]
 then
 	echo "A host RSA key is present, will remove it and an associated public key"
-	rm -fv /etc/ssh/id_rsa*
+	rm -fv /etc/ssh/ssh_host_rsa*
 fi
 
-if [ -f /etc/ssh/id_dsa ]
+if [ -f /etc/ssh/ssh_host_dsa ]
 then
 	echo "A host DSA key is present, will remove it and an associated public key"
-	rm -fv /etc/ssh/id_dsa*
+	rm -fv /etc/ssh/ssh_host_dsa*
 fi
 
-if [ -f /etc/ssh/id_ecdsa ]
+if [ -f /etc/ssh/ssh_host_ecdsa ]
 then
 	echo "A host ECDSA is present, will remove it and an associated public key"
-	rm -fv /etc/ssh/id_ecdsa*
+	rm -fv /etc/ssh/ssh_host_ecdsa*
 fi
 
 if ls ~/.ssh/id_* 1> /dev/null 2>&1; then
