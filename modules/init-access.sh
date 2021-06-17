9f8e81ea515d0531cf6556e8df7783e397fb3390
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2021-06-17 21:33:42 +0100

    Switching to azlondon3

--- modules/init-access.sh
+++ modules/init-access.sh
@@ -25,8 +25,10 @@ then
 	echo "will clean it up"
 	cat /dev/null >|  ~mikhail/.ssh/authorized_keys
 fi
-echo "appending mikhail@azlondon1 key to ~mikhail/.ssh/authorized_keys"
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFaTVQI0jD43nDrYOzDDzbIoA/Qe2TRFA6Eq8MbyqBCo mikhail@azlondon1.bluestar.cloud">~mikhail/.ssh/authorized_keys
+#echo "appending mikhail@azlondon1 key to ~mikhail/.ssh/authorized_keys"
+#echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFaTVQI0jD43nDrYOzDDzbIoA/Qe2TRFA6Eq8MbyqBCo mikhail@azlondon1.bluestar.cloud">~mikhail/.ssh/authorized_keys
+echo "appending mikhail@azlondon3 key to ~mikhail/.ssh/authorized_keys"
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3l4P/tbD0SjOEt6l8GmTAb5ZKOiHgs7I9aywVwwA9S mikhail@azlondon3.bluestar.cloud">~mikhail/.ssh/authorized_keys
 chmod -v 644 ~mikhail/.ssh/authorized_keys
 chown -vR mikhail:mikhail  ~mikhail/.ssh/authorized_keys
 
@@ -52,26 +54,26 @@ if [ -f ~mikhail/.ssh/id_ed25519 ]
 then
 	echo "A private Ed25519 key is present, skipping ssh-keygen stage"
 else
-	ssh-keygen -q -N "bluestar.cloud" -t ed25519 -f ~mikhail/.ssh/id_ed25519
+	sudo -u mikhail ssh-keygen -q -N "bluestar.cloud" -t ed25519 -f ~mikhail/.ssh/id_ed25519
 fi
 
-AZLONDON="$(dig +short azlondon1.bluestar.cloud | tail -n1)"
+AZLONDON="$(dig +short azlondon3.bluestar.cloud | tail -n1)"
 
 if [ -f /etc/hosts.allow ]
 then
 	echo "hosts.allow is present"
 
-	if ! grep azlondon1  /etc/hosts.allow; then
-   		echo "hosts.allow doesn't include azlondon, will append it"
+	if ! grep azlondon3  /etc/hosts.allow; then
+   		echo "hosts.allow doesn't include azlondon3, will append it"
 		sed -i "1 i\sshd : ${AZLONDON} : allow" /etc/hosts.allow
-		sed -i '1 i\sshd : azlondon1.bluestar.cloud : allow' /etc/hosts.allow
+		sed -i '1 i\sshd : azlondon3.bluestar.cloud : allow' /etc/hosts.allow
 	fi
 fi
 
 systemctl is-active --quiet firewalld
 if [ $? -eq 0 ]
 then
-	echo "firewalld is active, will add a rule to allow SSH from azlondon1"
+	echo "firewalld is active, will add a rule to allow SSH from azlondon3"
 	firewall-cmd --permanent --zone=trusted --add-source="${AZLONDON}"
 	echo "now firewalld has following settings for the trusted zone"
 	firewall-cmd --zone=trusted --list-all
