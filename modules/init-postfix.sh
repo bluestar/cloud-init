6d94fca7b7cd71ab2eb071e939a04c0e170ad4ae
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2022-01-01 23:21:08 +0000

    skip postfix setup if it is not configured

--- modules/init-postfix.sh
+++ modules/init-postfix.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-systemctl enable postfix ; sudo systemctl start postfix
+if [ -x "$(command -v postfix)" ]; then
+    systemctl enable postfix ; sudo systemctl start postfix
 
-sed -i 's/^#\?root:.*$/root: support@bluestar.cloud/g' /etc/aliases
-echo "updated /etc/aliases, now it has following line:"
-grep root: /etc/aliases
-echo "reloading postfix aliases database"
-newaliases
+    sed -i 's/^#\?root:.*$/root: support@bluestar.cloud/g' /etc/aliases
+    echo "updated /etc/aliases, now it has following line:"
+    grep root: /etc/aliases
+    echo "reloading postfix aliases database"
+    newaliases
+else
+    echo "postfix not found, postfix configuration steps skipped"
+fi
