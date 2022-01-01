633e6cefe6fb54dbde1fe18fe92e32fc30509c37
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2022-01-01 17:17:11 +0000

    Minor enhancement

--- modules/init-apt.sh
+++ modules/init-apt.sh
@@ -3,7 +3,6 @@
 if [ -x "$(command -v apt)" ]; then
   echo "init apt module"
   apt -y install jq
-  apt -y install dnsutils
-  apt -y install telnet net-tools htop nload
+  apt -y install traceroute lsof telnet net-tools dnsutils nmap wget htop nload
   apt -y autoremove
 fi
