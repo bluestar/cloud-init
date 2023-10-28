3687a43cb8962a1a29204f40d6c51e8cf864a473
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2023-10-28 18:14:13 +0100

    Added support for AlmaLinux 9

--- modules/init-dnf.sh
+++ modules/init-dnf.sh
@@ -7,7 +7,21 @@ if [ -x "$(command -v dnf)" ]; then
   dnf -y update
   dnf -y install jq
   echo jq version `jq --version` is installed
+  dnf -y install lsb_release
+
+  # Get the operating system name and version
+  os_name=$(lsb_release -si)
+  os_major_version=$(lsb_release -sr | cut -d'.' -f1)
+
   dnf -y install bind-utils
   dnf -y install telnet net-tools htop nload
-  dnf -y install postfix mailx  
+
+  # AlmaLinux 9 has s-nail instead of mailx
+  if [[ "$os_name" == "AlmaLinux" && "$os_major_version" == "9" ]]; then
+    dnf -y install postfix s-nail 
+  elif [[ "$os_name" == "AlmaLinux" && "$os_major_version" == "8" ]]; then
+    dnf -y install postfix mailx
+  else
+    dnf -y install postfix
+  fi
 fi
